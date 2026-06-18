3134b09c4bd9d9ddf38b4b604ea0cb8866dc4e69
praveen kumar gupta <praveenkumargupta640@gmail.com>
2026-06-18 15:04:52 +0530

    add gunicorn for deployment

--- lms/settings.py
+++ lms/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-)n8s630@6s9xi5@_7@_8=^f(7k#&9sa5tkd*wlldo5$7z*a_!h'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # APPLICATIONS
@@ -90,6 +90,7 @@ USE_TZ = True
 
 # STATIC FILES (CSS / JS)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
