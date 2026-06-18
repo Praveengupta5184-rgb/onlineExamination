3ab2115a5b766a9a5e36a7d249676236eaceb5ba
praveen kumar gupta <praveenkumargupta640@gmail.com>
2026-06-18 16:08:27 +0530

    Configure WhiteNoise for static files

--- lms/settings.py
+++ lms/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,7 +90,7 @@ USE_TZ = True
 
 
 # STATIC FILES (CSS / JS)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
